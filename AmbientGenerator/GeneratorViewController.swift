61bfbab3eaae99c19c57b10993d3ac85bc1758b7
VSMDURGAPRASAD <sai1996.vinukonda@gmail.com>
2019-03-02 00:27:24 -0600

    Updated Melody,Chaos,Energy text values with the slider in view controller

--- AmbientGenerator/GeneratorViewController.swift
+++ AmbientGenerator/GeneratorViewController.swift
@@ -12,11 +12,39 @@ class FirstViewController: UIViewController {
     
     @IBOutlet weak var droneLBL: UILabel!
     
+    @IBOutlet weak var melodyLBL: UILabel!
+    
+    @IBOutlet weak var chaosLBL: UILabel!
+    @IBOutlet weak var energyLBL: UILabel!
+    
+    @IBAction func chaosSlider(_ sender: UISlider) {
+        chaosLBL.text=String(Int(sender.value))
+    }
+    
+    @IBAction func melodySlider(_ sender: UISlider) {
+        melodyLBL.text=String(Int(sender.value))
+    }
     
     @IBAction func droneSlider(_ sender: UISlider) {
-        
-        droneLBL.text = String(sender.value)
+        droneLBL.text = String(Int(sender.value))
     }
+    
+    
+    
+    @IBAction func energySlider(_ sender: UISlider) {
+        energyLBL.text=String(Int(sender.value))
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
 
