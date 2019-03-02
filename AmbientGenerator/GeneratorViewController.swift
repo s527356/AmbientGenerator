9440193ff8e084e9fb8c2abe966eea7c5bb98150
VSMDURGAPRASAD <sai1996.vinukonda@gmail.com>
2019-03-02 00:02:08 -0600

    Updated the Min and Max  Values to the Drone Slider

--- AmbientGenerator/GeneratorViewController.swift
+++ AmbientGenerator/GeneratorViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var droneLBL: UILabel!
+    
+    
+    @IBAction func droneSlider(_ sender: UISlider) {
+        
+        droneLBL.text = String(sender.value)
+    }
+    
+    
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
