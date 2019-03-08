39b9f3a9fbbdf25e6760252abc81b57e6da50022
Venkata Ajit Kumar <s531497@nwmissouri.edu>
2019-03-07 18:05:27 -0600

    Added music to loop view

--- AmbientGenerator/LoopViewController.swift
+++ AmbientGenerator/LoopViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SecondViewController: UIViewController {
-
-	override func viewDidLoad() {
+    
+    var musicEffect: AVAudioPlayer = AVAudioPlayer()
+    
+   override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
-	}
+	let musicFile = Bundle.main.path(forResource: "sound1", ofType: ".mp3")
+    do {
+        try musicEffect = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile!))
+    }
+    catch {
+        print(error)
+    }
 
+}
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+}
+    
+    @IBAction func playMusic(_ sender: Any) {
+    musicEffect.play()
+    }
+
+
+    @IBAction func stopMusic(_ sender: Any) {
+    musicEffect.stop()
+    }
+    
 
 }
 
