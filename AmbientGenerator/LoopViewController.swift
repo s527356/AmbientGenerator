fd384c64b165ece2986e193e0886eca5381a14e8
Venkata Ajit Kumar <s531497@nwmissouri.edu>
2019-03-09 21:01:45 -0600

    Added totally 3 music files in loop view

--- AmbientGenerator/LoopViewController.swift
+++ AmbientGenerator/LoopViewController.swift
@@ -12,33 +12,62 @@ import AVFoundation
 class SecondViewController: UIViewController {
     
     var musicEffect: AVAudioPlayer = AVAudioPlayer()
+    var musicEffect2: AVAudioPlayer = AVAudioPlayer()
+    var musicEffect3: AVAudioPlayer = AVAudioPlayer()
     
-   override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	let musicFile = Bundle.main.path(forResource: "sound1", ofType: ".mp3")
-    do {
-        try musicEffect = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile!))
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let musicFile = Bundle.main.path(forResource: "sound1", ofType: ".mp3")
+        let musicFile2 = Bundle.main.path(forResource: "Sound2", ofType: ".mp3")
+        let musicFile3 = Bundle.main.path(forResource: "Sound3", ofType: ".mp3")
+        do {
+            try musicEffect = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile!))
+            try musicEffect2 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile2!))
+            try musicEffect3 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile3!))
+            
+        }
+        catch {
+            print(error)
+        }
+        
     }
-    catch {
-        print(error)
-    }
-
-}
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-}
+    }
     
     @IBAction func playMusic(_ sender: Any) {
-    musicEffect.play()
-    }
-
-
-    @IBAction func stopMusic(_ sender: Any) {
-    musicEffect.stop()
+        musicEffect.play()
+        musicEffect2.stop()
+        musicEffect3.stop()
     }
     
-
+    
+    @IBAction func stopMusic(_ sender: Any) {
+        musicEffect.stop()
+    }
+    
+    @IBAction func playMusic2(_ sender: Any) {
+        musicEffect2.play()
+        musicEffect.stop()
+        musicEffect3.stop()
+    }
+    
+    @IBAction func stopMusic2(_ sender: Any) {
+        musicEffect2.stop()
+    }
+    
+    
+    @IBAction func playMusic3(_ sender: Any) {
+        musicEffect3.play()
+        musicEffect2.stop()
+        musicEffect.stop()
+    }
+    
+    @IBAction func stopMusic3(_ sender: Any) {
+        musicEffect3.stop()
+    }
+    
 }
 
