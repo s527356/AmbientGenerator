6eb0e1c1388929c4e0d191257be538082024b953
Keenan <keenanpb@gmail.com>
2019-03-11 09:16:34 -0500

    Add AudioKit test

--- AmbientGenerator/GeneratorViewController.swift
+++ AmbientGenerator/GeneratorViewController.swift
@@ -7,52 +7,59 @@
 //
 
 import UIKit
+import AudioKit
 
 class FirstViewController: UIViewController {
-    
-    @IBOutlet weak var droneLBL: UILabel!
-    
-    @IBOutlet weak var melodyLBL: UILabel!
-    
-    @IBOutlet weak var chaosLBL: UILabel!
-    @IBOutlet weak var energyLBL: UILabel!
-    
-    @IBAction func chaosSlider(_ sender: UISlider) {
-        chaosLBL.text=String(Int(sender.value))
-    }
-    
-    @IBAction func melodySlider(_ sender: UISlider) {
-        melodyLBL.text=String(Int(sender.value))
-    }
-    
-    @IBAction func droneSlider(_ sender: UISlider) {
-        droneLBL.text = String(Int(sender.value))
-    }
-    
-    
-    
-    @IBAction func energySlider(_ sender: UISlider) {
-        energyLBL.text=String(Int(sender.value))
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
+	
+	let audioGenerator = AudioGenerator.shared
+	
+	@IBOutlet weak var droneLBL: UILabel!
+	
+	@IBOutlet weak var melodyLBL: UILabel!
+	
+	@IBOutlet weak var chaosLBL: UILabel!
+	@IBOutlet weak var energyLBL: UILabel!
+	
+	@IBAction func chaosSlider(_ sender: UISlider) {
+		chaosLBL.text=String(Int(sender.value))
+	}
+	
+	@IBAction func melodySlider(_ sender: UISlider) {
+		melodyLBL.text=String(Int(sender.value))
+	}
+	
+	@IBAction func droneSlider(_ sender: UISlider) {
+		droneLBL.text = String(Int(sender.value))
+	}
+	
+	
+	
+	@IBAction func energySlider(_ sender: UISlider) {
+		energyLBL.text=String(Int(sender.value))
+	}
+	
+	var oscillator = AKOscillator()
+	@IBAction func testBN(_ sender: Any) {
+		if oscillator.isPlaying {
+			oscillator.stop()
+		} else {
+			oscillator.amplitude = random(in: 0.5...1)
+			oscillator.frequency = random(in: 220...880)
+			oscillator.start()
+		}
+	}
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		AudioKit.output = AKMixer(oscillator)
+		do {
+			try AudioKit.start()
+		} catch {
+			print("Error starting AudioKit")
+		}
 	}
-
-
+	
+	
 }
 
--- AmbientGenerator/generator.swift
+++ AmbientGenerator/generator.swift
@@ -8,47 +8,63 @@
 
 import Foundation
 
-struct Generator {
-    static var shared = Generator()
-    var droneValue = 0
-    var melodyValue = 0
-    var chaosValue = 0
-    var energyValue = 0
-    
-    
-    
-    
-    private init(){}
-    func slideChange()->Int {
-        if droneValue >= 0 && droneValue <= 100 {
-            return 50
-        }
-        else {
-            return 0
-        }
-    }
-    func melodyslideChange()->Int {
-        if energyValue >= 0 && energyValue <= 100 {
-            return 50
-        }
-        else {
-            return 0
-        }
-    }
-    func chaosslideChange()->Int {
-        if chaosValue >= 0 && chaosValue <= 100 {
-            return 50
-        }
-        else {
-            return 0
-        }
-    }
-    func energyslideChange()->Int {
-        if energyValue >= 0 && energyValue <= 100 {
-            return 50
-        }
-        else {
-            return 0
-        }
-    }
+import AudioKit
+
+class AudioGenerator {
+	
+	static let shared = AudioGenerator()
+	
+	func test() {
+		var oscillator = AKOscillator()
+		AudioKit.output = oscillator      //Tell AudioKit what to output
+		do {
+			try AudioKit.start()  //Start up AudioKit
+		} catch {
+			print("Error Starting AudioKit")
+		}
+		oscillator.start()  //Start the oscillator
+		oscillator.frequency = random(in: 220...880)  //Set oscillator parameters
+	}
+}
+
+struct GeneratorController {
+	static var shared = GeneratorController()
+	var droneValue = 0
+	var melodyValue = 0
+	var chaosValue = 0
+	var energyValue = 0
+	
+	private init(){}
+	func slideChange()->Int {
+		if droneValue >= 0 && droneValue <= 100 {
+			return 50
+		}
+		else {
+			return 0
+		}
+	}
+	func melodyslideChange()->Int {
+		if energyValue >= 0 && energyValue <= 100 {
+			return 50
+		}
+		else {
+			return 0
+		}
+	}
+	func chaosslideChange()->Int {
+		if chaosValue >= 0 && chaosValue <= 100 {
+			return 50
+		}
+		else {
+			return 0
+		}
+	}
+	func energyslideChange()->Int {
+		if energyValue >= 0 && energyValue <= 100 {
+			return 50
+		}
+		else {
+			return 0
+		}
+	}
 }
