166420427a1c4568f842c42f0aeb5d32b78c6cdb
Patle,Srikar <s533986@ch140030m.nwmissouri.edu>
2019-03-11 19:26:13 -0500

    created timer view tab

--- AmbientGenerator/TimerViewController.swift
+++ AmbientGenerator/TimerViewController.swift
@@ -19,25 +19,12 @@ class TimerViewController: UIViewController {
     
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var StopButton: UIButton!
-    var timer = Timer()
-    var isTimerRunning = false
-    var counter = 0.0
+    
     
     
     @IBAction func Pause(_ sender: Any) {
         
-        Reset.isEnabled = true
-        startButton.isEnabled = true
-        Pausebutton.isenabled = false
-        
-        
-        Reset.alpha = 1.0
-        startButton.alpha = 1.0
-        Pausebutton.alpha = 0.5
-        
-        
-        isTimerRunning = false
-        timer.invalidate()
+        print("Pause")
     
     }
     
@@ -46,82 +33,19 @@ class TimerViewController: UIViewController {
     }
     
     @IBAction func ResetButton(_ sender: Any) {
-       timer.invalidate()
-        isTimerRunning = false
-        counter = 0.0
-        
-        timerLabel.text="00.00.00"
-        Reset.isEnabled = false
-        Pausebutton.isEnabled = false
-        startButton.isEnabled = true
-        
-        
-        Reset.alpha = 0.5
-        startButton.alpha = 1.0
-        Pausebutton.alpha = 0.5
+       print("ResetButton")
         
     }
     
     @IBAction func Start(_ sender: Any) {
-        if !isTimerRunning{
-            timer=Timer.scheduledtimer(timeInterval: 0.1,target: self,selector: #selector(runTimer), userInfo: nil,repeats: true)
-            isTimerRunning = true
-            
-            Reset.isEnabled = true
-            Pausebutton.isEnabled = true
-            startButton.isEnabled=false
-            
-            
-            
-            Reset.alpha = 1.0
-            startButton.alpha = 0.5
-            Pausebutton.alpha = 1.0
-            
+        print("Start")
         }
         
-    }
-    @objc func runTimer(){
-        counter += 0.1
-        let flooredCounter = Int(floor(counter))
-        let hour = flooredCounter / 3600
-        
-        let minute = (flooredCounter % 3600) / 60
-        var minuteString = "\(minute)"
-        if minute < 10{
-            minuteString = "0\(minute)"
-        }
-        let second = (flooredCounter % 3600) % 60
-        var secondString = "\(second)"
-        if second < 10{
-            secondString = "0\(second)"
-        }
-        let decisecond = string(format: "%1f", counter).components(seperatedBy: ".").last!
-        timerlabel.text = "\(hour):\(minuteString):\(secondString).\(decisecond)"
-    }
     
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        Reset.isEnabled = false
-        Pausebutton.isEnabled = false
-        startButton.isEnabled=true
-        Timer.layer.cornerRadius = 5.0
-        Timer.layer.marksToBounds = true
-        
-        
-        Reset.layer.cornerRadius = 4.0
-        Reset.layer.masksToNounds = true
-        Reset.isEnabled = false
-        Reset.alpha = 0.5
-        
-        startButton.layer.cornerradius = startButton.bounds.width / 2.0
-        startButton.layer.masksToBounds = true
-        // Do any additional setup after loading the view.
-        
-        Pausebutton.layer.cornerRadius = Pausebutton.bounds.width / 2.0
-        Pausebutton.layer.masksToBounds = true
-        Pausebutton.isEnabled = false
-        Pausebutton.alpha = 0.5
         
     }
     
