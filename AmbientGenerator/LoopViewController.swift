703db6f8ed5d33fc839efaa18dea6c88b0aa40c2
Srikar Patle <student@ch140017m.nwmissouri.edu>
2019-04-04 20:50:06 -0500

    Added functionality for start and stop button in timer.

--- AmbientGenerator/LoopViewController.swift
+++ AmbientGenerator/LoopViewController.swift
@@ -10,35 +10,38 @@ import UIKit
 import AVFoundation
 
 class SecondViewController: UIViewController {
-    
-    var musicEffect: AVAudioPlayer = AVAudioPlayer()
-    var musicEffect2: AVAudioPlayer = AVAudioPlayer()
-    var musicEffect3: AVAudioPlayer = AVAudioPlayer()
-    var musicEffect4: AVAudioPlayer = AVAudioPlayer()
-    var musicEffect5: AVAudioPlayer = AVAudioPlayer()
-    var musicEffect6: AVAudioPlayer = AVAudioPlayer()
+   
+    var value1:TimerViewController!
+//    var musicEffect: AVAudioPlayer = AVAudioPlayer()
+//    var musicEffect2: AVAudioPlayer = AVAudioPlayer()
+//    var musicEffect3: AVAudioPlayer = AVAudioPlayer()
+//    var musicEffect4: AVAudioPlayer = AVAudioPlayer()
+//    var musicEffect5: AVAudioPlayer = AVAudioPlayer()
+//    var musicEffect6: AVAudioPlayer = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let musicFile = Bundle.main.path(forResource: "sound1", ofType: ".mp3")
-        let musicFile2 = Bundle.main.path(forResource: "Sound2", ofType: ".mp3")
-        let musicFile3 = Bundle.main.path(forResource: "Sound3", ofType: ".mp3")
-        let musicFile4 = Bundle.main.path(forResource: "Sound4", ofType: ".mp3")
-        let musicFile5 = Bundle.main.path(forResource: "Sound5", ofType: ".mp3")
-        let musicFile6 = Bundle.main.path(forResource: "Sound6", ofType: ".mp3")
-        do {
-            try musicEffect = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile!))
-            try musicEffect2 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile2!))
-            try musicEffect3 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile3!))
-            try musicEffect4 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile4!))
-            try musicEffect5 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile5!))
-            try musicEffect6 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile6!))
-            
-        }
-        catch {
-            print(error)
-        }
+//        value1 = (self.tabBarController?.viewControllers![2] as! TimerViewController)
+//        print(value1.counter)
+//        // Do any additional setup after loading the view, typically from a nib.
+//        let musicFile = Bundle.main.path(forResource: "sound1", ofType: ".mp3")
+//        let musicFile2 = Bundle.main.path(forResource: "Sound2", ofType: ".mp3")
+//        let musicFile3 = Bundle.main.path(forResource: "Sound3", ofType: ".mp3")
+//        let musicFile4 = Bundle.main.path(forResource: "Sound4", ofType: ".mp3")
+//        let musicFile5 = Bundle.main.path(forResource: "Sound5", ofType: ".mp3")
+//        let musicFile6 = Bundle.main.path(forResource: "Sound6", ofType: ".mp3")
+//        do {
+//            try musicEffect = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile!))
+//            try musicEffect2 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile2!))
+//            try musicEffect3 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile3!))
+//            try musicEffect4 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile4!))
+//            try musicEffect5 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile5!))
+//            try musicEffect6 = AVAudioPlayer(contentsOf: URL (fileURLWithPath: musicFile6!))
+//
+//        }
+//        catch {
+//            print(error)
+//        }
         
     }
     
@@ -47,76 +50,104 @@ class SecondViewController: UIViewController {
     }
     
     @IBAction func playMusic(_ sender: Any) {
-        musicEffect.play()
-        musicEffect2.stop()
-        musicEffect3.stop()
+       Sounds.sounds.musicEffect.play()
+       Sounds.sounds.musicEffect2.stop()
+       Sounds.sounds.musicEffect3.stop()
+        //musicEffect.play()
+        //musicEffect2.stop()
+        //musicEffect3.stop()
     }
     
     
     @IBAction func stopMusic(_ sender: Any) {
-        musicEffect.stop()
+       Sounds.sounds.musicEffect.stop()
     }
     
     @IBAction func playMusic2(_ sender: Any) {
-        musicEffect2.play()
-        musicEffect.stop()
-        musicEffect3.stop()
+        Sounds.sounds.musicEffect2.play()
+        Sounds.sounds.musicEffect.stop()
+        Sounds.sounds.musicEffect.stop()
+//        musicEffect2.play()
+//        musicEffect.stop()
+//        musicEffect3.stop()
     }
     
     @IBAction func stopMusic2(_ sender: Any) {
-        musicEffect2.stop()
+        Sounds.sounds.musicEffect2.play()
     }
     
     
     @IBAction func playMusic3(_ sender: Any) {
-        musicEffect3.play()
-        musicEffect2.stop()
-        musicEffect.stop()
+        Sounds.sounds.musicEffect3.play()
+        Sounds.sounds.musicEffect2.stop()
+        Sounds.sounds.musicEffect.stop()
+//        musicEffect3.play()
+//        musicEffect2.stop()
+//        musicEffect.stop()
     }
     
     @IBAction func stopMusic3(_ sender: Any) {
-        musicEffect3.stop()
+        Sounds.sounds.musicEffect3.stop()
+        //musicEffect3.stop()
     }
     
     
     @IBAction func playMusic4(_ sender: Any) {
-        musicEffect4.play()
-        musicEffect2.stop()
-        musicEffect3.stop()
-        musicEffect.stop()
+        Sounds.sounds.musicEffect4.play()
+        Sounds.sounds.musicEffect2.stop()
+        Sounds.sounds.musicEffect3.stop()
+        Sounds.sounds.musicEffect.stop()
+//        musicEffect4.play()
+//        musicEffect2.stop()
+//        musicEffect3.stop()
+//        musicEffect.stop()
         
     }
     
     @IBAction func stopMusic4(_ sender: Any) {
-        musicEffect4.stop()
+        Sounds.sounds.musicEffect4.stop()
+//        musicEffect4.stop()
     }
     
     
     @IBAction func playMusic5(_ sender: Any) {
-        musicEffect5.play()
-        musicEffect4.stop()
-        musicEffect2.stop()
-        musicEffect3.stop()
-        musicEffect.stop()
+        Sounds.sounds.musicEffect5.play()
+        Sounds.sounds.musicEffect4.stop()
+        Sounds.sounds.musicEffect2.stop()
+        Sounds.sounds.musicEffect3.stop()
+        Sounds.sounds.musicEffect.stop()
+//        musicEffect5.play()
+//        musicEffect4.stop()
+//        musicEffect2.stop()
+//        musicEffect3.stop()
+//        musicEffect.stop()
     }
     
     @IBAction func stopMusic5(_ sender: Any) {
-        musicEffect5.stop()
+        Sounds.sounds.musicEffect5.stop()
+//        musicEffect5.stop()
     }
     
     
     @IBAction func playMusic6(_ sender: Any) {
-        musicEffect6.play()
-        musicEffect5.stop()
-        musicEffect4.stop()
-        musicEffect2.stop()
-        musicEffect3.stop()
-        musicEffect.stop()
+        Sounds.sounds.musicEffect6.play()
+        Sounds.sounds.musicEffect4.stop()
+        Sounds.sounds.musicEffect2.stop()
+        Sounds.sounds.musicEffect3.stop()
+        Sounds.sounds.musicEffect5.stop()
+        Sounds.sounds.musicEffect.stop()
+//        musicEffect6.play()
+//        musicEffect5.stop()
+//        musicEffect4.stop()
+//        musicEffect2.stop()
+//        musicEffect3.stop()
+//        musicEffect.stop()
     }
     
     
     @IBAction func stopMusic6(_ sender: Any) {
-        musicEffect6.stop()
+        Sounds.sounds.musicEffect6.stop()
+//        musicEffect6.stop()
     }
     
 }
--- AmbientGenerator/TimerViewController.swift
+++ AmbientGenerator/TimerViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 
 
 class TimerViewController: UIViewController {
-    
+
     
     @IBOutlet weak var Reset: UIButton!
     @IBOutlet weak var timer: UITextField!
@@ -27,47 +27,67 @@ class TimerViewController: UIViewController {
     @IBAction func Pause(_ sender: Any) {
         timer1.invalidate()
         print("Pause")
-    
+//        Sounds.sounds.musicEffect6.pause()
+//        Sounds.sounds.musicEffect4.pause()
+//        Sounds.sounds.musicEffect2.pause()
+//        Sounds.sounds.musicEffect3.pause()
+//        Sounds.sounds.musicEffect5.pause()
+//        Sounds.sounds.musicEffect.pause()
     }
     
     @IBAction func Stop(_ sender: Any) {
         if timer != nil
         {
-       timer1.invalidate()
+            timer1.invalidate()
+            Sounds.sounds.musicEffect6.stop()
+            Sounds.sounds.musicEffect4.stop()
+            Sounds.sounds.musicEffect2.stop()
+            Sounds.sounds.musicEffect3.stop()
+            Sounds.sounds.musicEffect5.stop()
+            Sounds.sounds.musicEffect.stop()
         }
     }
     
     @IBAction func ResetButton(_ sender: Any) {
-        if timer != nil
-        {
+        if timer != nil{
         timer.text = "\(0)"
        print("ResetButton")
+            Sounds.sounds.musicEffect6.stop()
+            Sounds.sounds.musicEffect4.stop()
+            Sounds.sounds.musicEffect2.stop()
+            Sounds.sounds.musicEffect3.stop()
+            Sounds.sounds.musicEffect5.stop()
+            Sounds.sounds.musicEffect.stop()
         }
     }
     
     @IBAction func Start(_ sender: Any) {
-        if timer != nil
-        {
+        
+        if timer != nil{
         print("Start")
         timer1.invalidate() // just in case this button is tapped multiple times
         // start the timer
         timer1 = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
         }
+        
         }
         
     @objc func timerAction() {
        // counter += 1
-        if timer != nil
-        {
+        if timer != nil{
         counter = Int(timer.text!)!
-        if counter != 0
-        {
+        if counter != 0{
         counter = counter - 1
         timer.text = "\(counter)"
         }
-        else
-        {
+        else{
           timer1.invalidate()
+            Sounds.sounds.musicEffect6.stop()
+            Sounds.sounds.musicEffect4.stop()
+            Sounds.sounds.musicEffect2.stop()
+            Sounds.sounds.musicEffect3.stop()
+            Sounds.sounds.musicEffect5.stop()
+            Sounds.sounds.musicEffect.stop()
         }
         }
     }
